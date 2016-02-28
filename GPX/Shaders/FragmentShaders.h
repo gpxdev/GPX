80ccda74807c47ad3ec2a339dfad75fdbff3d49e
George Coomber <georgecoomber@gmail.com>
2016-02-27 18:56:48 -0700

    Following tutorial from http://learnopengl.com/#!Getting-started/

---
+++ GPX/Shaders/FragmentShaders.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace std
+{
+
+static const GLchar* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 color;\n"
+    "void main()\n"
+    "{\n"
+    "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
+}
